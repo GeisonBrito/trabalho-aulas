e94253baf20c147249a72618e666b1b08435e2bc
GeisonBrito <geisonbrito@icloud.com>
2023-09-11 20:50:40 -0300

    first commit

---
+++ trabalho.py
@@ -0,0 +1,18 @@
+n1 = int(input("Digite o primeiro numero?"))
+n2 = int(input("Digite o segundo numero?"))
+
+print(" A soma dos dois numero é:",{n1 + n2})
+
+n3 = int(input("Digite o terceiro numero?"))
+n4 = int(input("Digite o quatro numero?"))
+
+print(" A soma dos dois numeros é:",{n3 + n4})
+
+soma  = []
+for numeros in range (0,4):
+    numero=int(input("Digite o numero?"))
+    soma.append(numero)
+
+print(soma[0] + soma[1])
+
+print(soma[2] + soma[3])                                                                                    
